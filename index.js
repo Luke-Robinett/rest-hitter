8053287f081547ccbdc80fa838e2c99f744ff42e
Luke Robinett <lukebootcamp@gmail.com>
2020-08-07 16:36:18 -0700

    Implemented saving and retrieving of sessions.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    updateHistory(getSavedSessions());
+
     $("#use-auth").on("change", function () {
         $("#credentials").toggleClass("d-none");
         $(".auth").prop("required", $(this).prop("checked"));
@@ -13,20 +15,22 @@ $(document).ready(function () {
     $("form").on("submit", function (event) {
         event.preventDefault();
 
+        // Gather our data from the form fields
         const method = $("#method").val();
         const endpoint = $("#endpoint").val().trim();
-        const useAuth = $("#use-auth").prop("checked");
-        const requestBody = $("#request-body").val().trim();
+        const requestBody = (method === "POST") ? $("#request-body").val().trim() : "";
         const hasBody = (requestBody.length > 0);
+        const useAuth = $("#use-auth").prop("checked");
+        const username = useAuth ? $("#username").val() : "";
+        const password = useAuth ? $("#password").val() : "";
+        const save = $("#save").prop("checked");
+        const sessionName = save ? $("#session-name").val().trim() : "";
 
         let ajaxParams = {
             type: method,
             url: encodeURI(endpoint),
             beforeSend: function (xhr) {
                 if (useAuth) {
-                    const username = $("#username").val().trim();
-                    const password = $("#password").val().trim();
-
                     xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
                 }
             }
@@ -34,6 +38,32 @@ $(document).ready(function () {
 
         if (method === "POST" && hasBody)
             ajaxParams.data = JSON.parse(requestBody);
+
+        if (save) {
+            // Get list of saved sessions so we can add to it
+            const savedSessions = getSavedSessions();
+
+            // Create a new object to store session data
+            let savedSession = {
+                id: savedSessions.length + 1,
+                name: sessionName,
+                method: method,
+                endpoint: endpoint,
+                requestBody: requestBody,
+                useAuth: useAuth,
+                username: username,
+                password: password
+            };
+
+            // Add the new session to the list
+            savedSessions.push(savedSession);
+
+            // Save the list to local storage with newly added session
+            localStorage.setItem("savedSessions", JSON.stringify(savedSessions));
+
+            // Update the history list
+            updateHistory(savedSessions);
+        }
 
         $.ajax(ajaxParams)
             .then(response => {
@@ -43,6 +73,57 @@ $(document).ready(function () {
             .catch(error => {
                 console.error(error);
                 $("#output").val(JSON.stringify(error, null, "\t"));
-            });
+            })
+            .finally($("#output").focus());
     });
+
+    $("#save").on("change", function () {
+        $("#session-p").toggleClass("d-none");
+        $("#session-name").prop("required", $(this).prop("checked"));
+    });
+
+    $(".history-link").click(function (event) {
+        event.preventDefault();
+
+        const id = $(this).prop("data-id");
+
+        const session = getSessionFromHistory(id);
+
+        if (!session) return;
+
+        $("#method").val(session.method);
+        $("#endpoint").val(session.endpoint);
+    });
+
+    function getSessionFromHistory(id) {
+        const savedSessions = getSavedSessions();
+        const session = savedSessions.find(s => s.id === id);
+
+        return (session !== undefined) ? session : null;
+    }
+
+    function getSavedSessions() {
+        const savedSessions = localStorage.getItem("savedSessions");
+
+        if (savedSessions == null) return [];
+        return JSON.parse(localStorage.getItem("savedSessions"));
+    }
+
+    function updateHistory(savedSessions) {
+        if (!Array.isArray(savedSessions)) return;
+
+        const historyList = $("#history");
+        historyList.empty();
+
+        savedSessions.forEach(session => {
+            const historyListItem = $("<li>")
+                .addClass("list-group-item");
+            const historyLink = $("<a href='#'>")
+                .addClass("history-link")
+                .prop("data-id", session.id)
+                .text(session.name);
+            historyListItem.append(historyLink);
+            historyList.prepend(historyListItem);
+        });
+    }
 });
