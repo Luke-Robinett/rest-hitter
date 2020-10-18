d2aa25bbdb163c9cb92ab3dadf5f1d705ff9d2c3
Luke Robinett <lukebootcamp@gmail.com>
2020-10-18 15:51:32 -0700

    Moved API calls to server side using axios.

---
+++ public/js/index.js
@@ -0,0 +1,210 @@
+$(document).ready(function () {
+	updateHistory();
+	resetForm();
+
+	$("#run").on("click", function (event) {
+		event.preventDefault();
+		submitOnClick(false);
+	});
+
+	$("#run-and-save").on("click", function (event) {
+		event.preventDefault();
+		submitOnClick(true);
+	});
+
+	$("#reset").on("click", function (event) {
+		event.preventDefault();
+		resetForm();
+	});
+
+	$("#history").on("click", ".history-link", function (event) {
+		event.preventDefault();
+		historyLinkOnClick(event.target);
+	});
+	$("#history").on("click", ".fa-remove", function (event) {
+		event.preventDefault();
+		removeIconOnClick(event.target);
+	});
+	$("#clear-history").on("click", function (event) {
+		event.preventDefault();
+		clearHistory();
+	});
+
+	$("#session-name").on("change", event => UpdateTitle($(event.target).val()));
+
+	function UpdateTitle(newTitle) {
+		$(document).attr("title", `${newTitle} | Rest Hitter`);
+	}
+
+	function getSessionFromHistory(id) {
+		const savedSessions = getSavedSessions();
+		const session = savedSessions.find(s => s.id === id);
+
+		return (session !== undefined) ? session : null;
+	}
+
+	function getSavedSessions() {
+		const savedSessions = localStorage.getItem("savedSessions");
+
+		return savedSessions
+			? JSON.parse(savedSessions)
+			: [];
+	}
+
+	function updateHistory() {
+		$("#clear-history").addClass("d-none");
+
+		const savedSessions = getSavedSessions();
+
+		if (savedSessions.length > 0) {
+			$("#clear-history").removeClass("d-none");
+		}
+
+		$("#history").empty();
+
+		savedSessions.forEach(session => {
+			$("#history").prepend(
+				$("<div class='row' role='listitem'>").append(
+					$("<div class='col-6'>").append(
+						$(`<a href="#" class="history-link" data-id=${session.id}>${session.name}</a>`)
+					),
+					$("<div class='col-6'>").append(
+						$(`<a href="#" class="fa fa-remove text-danger" data-id=${session.id}><span class="sr-only">Remove ${session.name}</span></a>`)
+					)
+				)
+			);
+		});
+	}
+
+	function submitOnClick(saveFlag) {
+		// Gather form data
+		const formData = getFormData();
+
+		// Send form data to the back end API which will call the endpoint using axios
+		$.post("/api", formData)
+			.then(response => {
+				console.log(response);
+				$("#output").val(JSON.stringify(response, null, "\t"));
+			})
+			.catch(error => {
+				console.log(error);
+				$("#output").val(JSON.stringify(error, null, "\t"));
+				alert("There was an error.");
+			})
+			.always(() => {
+				if (saveFlag) {
+					saveSession(formData);
+					updateHistory();
+				}
+			});
+	}
+
+	function getFormData() {
+		return {
+			name: $("#session-name").val().trim(),
+			method: $("#method").val(),
+			endpoint: $("#endpoint").val().trim(),
+			body: $("#request-body").val(),
+			username: $("#username").val().trim(),
+			password: $("#password").val().trim()
+		};
+	}
+
+	function setFormData(formData) {
+		const {
+			name = generateSessionName(),
+			method = "GET",
+			endpoint = "",
+			body = "",
+			username = "",
+			password = ""
+		} = formData;
+
+		$("#session-name").val(name);
+		$("#method").val(method);
+		$("#endpoint").val(endpoint);
+		$("#request-body").val(body);
+		$("#username").val(username);
+		$("#password").val(password);
+		$("#output").val("");
+
+		UpdateTitle(name);
+	}
+
+	function saveSession(formData) {
+		let savedSession = {};
+
+		// Get list of saved sessions so we can add to it
+		const savedSessions = getSavedSessions();
+
+		// Search to see if there's already a session with the chosen name
+		const existingSessionIndex = savedSessions.findIndex(s => s.name.toLowerCase() === formData.name.toLowerCase());
+
+		// If a session with that name already exists, save new entries to it
+		if (existingSessionIndex >= 0) {
+			savedSession = {
+				id: savedSessions[existingSessionIndex].id,
+				...formData
+			};
+			savedSessions[existingSessionIndex] = savedSession;
+		} else {
+			savedSession = {
+				id: generateNewID(),
+				...formData
+			};
+
+			// Add the new session to the list
+			savedSessions.push(savedSession);
+		}
+
+		// Save the list to local storage with newly added session
+		localStorage.setItem("savedSessions", JSON.stringify(savedSessions));
+	}
+
+	function historyLinkOnClick(clickedLink) {
+		// Get the unique ID of the clicked link
+		const id = parseInt($(clickedLink).attr("data-id"));
+
+		// Find session with this ID
+		const session = getSessionFromHistory(id);
+
+		// If no session was found, exit the function
+		if (!session) {
+			return;
+		}
+
+		// Populate the form with values from the saved session
+		setFormData(session);
+	}
+
+	function generateNewID() {
+		// If no saved sessions yet, returns ID of 1
+		if (getSavedSessions().length === 0) return 1;
+
+		// Otherwise returns a new ID that is 1 greater than the highest existing ID
+		return getSavedSessions().map(session => session.id)
+			.sort((id1, id2) => id2 - id1)[0] + 1;
+	}
+
+	function generateSessionName() {
+		return `Untitled Session ${getSavedSessions().filter(session => /^untitled session \d*$/i.test(session.name)).length + 1}`;
+	}
+
+	function resetForm() {
+		setFormData({ name: generateSessionName() });
+	}
+
+	function clearHistory() {
+		if (confirm("Clear all saved sessions?")) {
+			localStorage.removeItem("savedSessions");
+			updateHistory();
+		}
+	}
+
+	function removeIconOnClick(clickedIcon) {
+		const id = parseInt($(clickedIcon).attr("data-id"));
+		const filteredSessions = getSavedSessions().filter(session => session.id !== id);
+		localStorage.setItem("savedSessions", JSON.stringify(filteredSessions));
+		updateHistory();
+	}
+});
--- server.js
+++ server.js
@@ -1,20 +1,55 @@
 const express = require("express");
-const { json } = require("express");
-const PORT = 3000;
+const axios = require("axios");
+const https = require("https");
+const path = require("path");
+const { Agent } = require("http");
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static("./"));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.sendFile("./index.html"));
-app.post("/", (req, res) => {
-    console.log(JSON.stringify(req.body));
-    res.json(req.body);
+// Serve the html page
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+
+// API POST route
+app.post("/api", (req, res) => {
+    const { method, endpoint, body, username, password } = req.body;
+
+    // Configure the API request
+    let axiosParams = {
+        method: method,
+        url: encodeURI(endpoint)
+    };
+
+    if (process.env.NODE_ENV === "development") {
+        axiosParams.httpsAgent = new https.Agent({
+            rejectUnauthorized: false,
+        });
+    }
+
+    if (username.lenth > 0 && password.lenth > 0) {
+        axiosParams.auth = {
+            username: username,
+            password: password
+        };
+    }
+
+    if (body.lenth > 0) {
+        axiosParams.data = JSON.parse(body);
+    }
+
+    // Make the API call
+    axios(axiosParams)
+        .then(response => {
+            console.log(response.data);
+            res.json(response.data).status(200);
+        })
+        .catch(error => {
+            console.error(error);
+            res.json(error).status(500);
+        });
 });
 
-app.listen(3000, error => {
-    if (error)
-        return console.error(error);
-    console.log(`Listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
